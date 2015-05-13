5cd6c1b3b8a6cf1c74960632d21e33e6e60acb8e
hespert <hespert@indra.es>
2015-05-13 11:50:03 +0200

    Add show splash config.

--- app/src/main/java/org/holylobster/nuntius/activity/SplashScreenActivity.java
+++ app/src/main/java/org/holylobster/nuntius/activity/SplashScreenActivity.java
@@ -24,33 +24,52 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import org.holylobster.nuntius.R;
 
 
-public class SplashScreenActivity extends Activity {
+public class SplashScreenActivity extends Activity implements Runnable {
+
+    private static final String debugTag = "SplashScreenActivity";
+
+    private boolean gotIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+        //Get SharedPreferences
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-        final boolean gotIt = settings.getBoolean("gotIt", false);
+        gotIt = settings.getBoolean("gotIt", false);
+        boolean enabledSplash = settings.getBoolean("enable_splash", true);
 
-        ((GradientDrawable) findViewById(R.id.splash).getBackground().getCurrent())
-                .setGradientRadius(getResources().getDimension(
-                        R.dimen.gradient_radius));
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Class activityClass = gotIt ? SettingsActivity.class : WelcomeActivity.class;
-                Intent i = new Intent(SplashScreenActivity.this, activityClass);
-                startActivity(i);
-                finish();
-            }
-        }, 2000);
+        //If enabledSplash is true show Splash else launch the next Activity
+        if (enabledSplash) {
+            setContentView(R.layout.activity_splash);
+            ((GradientDrawable) findViewById(R.id.splash).getBackground().getCurrent())
+                    .setGradientRadius(getResources().getDimension(
+                            R.dimen.gradient_radius));
+            new Handler().postDelayed(this, 2000);
+        } else {
+            launchNextActivity();
+        }
     }
 
+    /**
+     * Launch the next Activity
+     * Launch WelcomeActivity if gotIt is false;
+     * Launch SettingsActivity if gotit is true;
+     */
+    private void launchNextActivity() {
+        Class activityClass = gotIt ? SettingsActivity.class : WelcomeActivity.class;
+        Intent i = new Intent(SplashScreenActivity.this, activityClass);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public void run() {
+        launchNextActivity();
+    }
 }
